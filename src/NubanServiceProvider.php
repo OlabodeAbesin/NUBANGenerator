733fdc6858176f77a078ce0657052a1cb8e6e3a8
Olabode Abesin <Olabode.abesin@sparkle.ng>
2019-07-30 17:45:52 +0100

    removed references

--- src/NubanServiceProvider.php
+++ src/NubanServiceProvider.php
@@ -14,8 +14,6 @@ class NubanServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
-        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
         //
     }
 
